11b28c840804cc9e6e43404e4045fca926cf6687
Markus Ebner <hiwatari.seiji@gmail.com>
2022-02-16 16:49:28 +0100

    Add ScanResultSet counter to Wifi sensor

--- src/main/java/de/fhws/indoor/libsmartphonesensors/sensors/WiFi.java
+++ src/main/java/de/fhws/indoor/libsmartphonesensors/sensors/WiFi.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.net.wifi.ScanResult;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 import de.fhws.indoor.libsmartphonesensors.ASensor;
 import de.fhws.indoor.libsmartphonesensors.SensorType;
@@ -17,6 +18,7 @@ import de.fhws.indoor.libsmartphonesensors.helpers.WifiScanProvider;
 public class WiFi extends ASensor implements WifiScanProvider.WifiScanCallback {
 
 	private final WifiScanProvider wifiScanProvider;
+	private AtomicLong scanResultCnt = new AtomicLong(0);
 
 	public WiFi(WifiScanProvider wifiScanProvider) {
 		this.wifiScanProvider = wifiScanProvider;
@@ -25,16 +27,19 @@ public class WiFi extends ASensor implements WifiScanProvider.WifiScanCallback {
 	@Override
 	public void onResume(Activity act) {
 		wifiScanProvider.registerCallback(this);
+		scanResultCnt.set(0);
 	}
 
 	@Override
 	public void onPause(Activity act) {
 		wifiScanProvider.unregisterCallback(this);
+		scanResultCnt.set(0);
     }
 
 	@Override
 	public void onScanResult(List<ScanResult> scanResults) {
 		final StringBuilder sb = new StringBuilder(1024);
+		scanResultCnt.incrementAndGet();
 		for(final ScanResult sr : scanResults) {
 			sb.append(Helper.stripMAC(sr.BSSID)).append(';');
 			sb.append(sr.frequency).append(';');
@@ -42,5 +47,14 @@ public class WiFi extends ASensor implements WifiScanProvider.WifiScanCallback {
 			listener.onData(SensorType.WIFI, sr.timestamp * 1000, sb.toString());
 			sb.setLength(0);
 		}
+	}
+
+	/**
+	 * Get the number of scan results (batches of measurements from the OS) that were received since
+	 * the sensor was started.
+	 * @return Number of scan results received since sensor start.
+	 */
+	public Long getScanResultCount() {
+		return scanResultCnt.get();
 	}
 }
